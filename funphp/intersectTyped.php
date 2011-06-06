62640ae173244036b9249c72c8982a19c48037ec
Jani Hartikainen <jani@jjh.fi>
2011-06-06 14:16:29 +0300

    Added arrayify to intersectTyped so it works with objects

--- funphp/intersectTyped.php
+++ funphp/intersectTyped.php
@@ -1,8 +1,10 @@
 <?php
 namespace funphp;
+require_once 'funphp/arrayify.php';
 
 function intersectTyped($master, $other) {
     $intersection = array();
+    $other = arrayify($other);
 
     foreach($master as $m) {
 	if(in_array($m, $other, true)) {
